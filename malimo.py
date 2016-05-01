3f45b3ed59e4c5f9280c46ba6e29a92a4e9819de
Malcolm Monroe <malcolmmonroe@Malcolm-Monroe-MacBook-Pro.local>
2016-04-30 18:36:11 -0700

    Content and Style added

--- malimo.py
+++ malimo.py
@@ -1,10 +1,20 @@
+import os 
 from flask import Flask, render_template
+import json 
+import requests 
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	#return str('Something')
-    return render_template('home.html') 
+def get_malimo_home():
+
+	workFilePath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/media/work_experience.json') 
+	with open(workFilePath, 'r') as work_file:
+		content = work_file.read()
+		workData = json.loads(content)
+		work_file.close()
+
+	return render_template('home.html', work_data=workData) 
 
 if __name__ == '__main__':
     app.run(debug=True)
