0738094772ca50a95d260edb048792b8bbc83acc
Malcolm Monroe <malcolmian.monroe@gmail.com>
2016-05-29 19:24:58 -0700

    Style is finally updated. About time huh?

--- malimo.py
+++ malimo.py
@@ -1,9 +1,18 @@
 import os 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message as MailMessage
 import json 
 import requests 
 
 app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'malcolmian.monroe@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Fatman68'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 @app.route('/')
 def get_malimo_home():
 
@@ -13,7 +22,22 @@ def get_malimo_home():
 		workData = json.loads(content)
 		work_file.close()
 
-	return render_template('home.html', work_data=workData) 
+	return render_template('index.html', work_data=workData) 
+
+
+
+@app.route('/sendMessage', methods=['POST']) 
+def messenger():
+	inquirerName = request.form['name']
+	inquirerEmail = request.form['email']
+	inquirerMessage = request.form['message']
+	msg = MailMessage(inquirerMessage,
+              	sender=inquirerEmail,
+		recipients=["malcolmian.monroe@gmail.com"])
+	mail.send(msg)
+	return redirect(url_for('get_malimo_home'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
